73703add7a4aeacccfc81bb813bd4d4c47a9444a
lasith chamuditha <125144995+lasith33@users.noreply.github.com>
2025-01-27 23:39:10 +0530

    add login button

--- lib/users/authentication/login_screen.dart
+++ lib/users/authentication/login_screen.dart
@@ -43,131 +43,166 @@ class _LoginScreenState extends State<LoginScreen>
                   ),
 
                   //login screen sign-in form
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white24,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(60),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 8,
-                          color: Colors.black26,
-                          offset: Offset(0, -3),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white24,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(60),
                         ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        Form(
-                          key: formKey,
-                          child: Column(
-                            children: [
-                              //email
-                              TextFormField(
-                                controller: emailController,
-                                validator: (val) => val =="" ? "Please write email" : null,
-                                decoration: InputDecoration(
-                                  prefixIcon: const Icon(
-                                    Icons.email,
-                                    color: Colors.black,
-                                  ),
-                                  hintText: "email...",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0, -3),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 30, 30, 8),
+                        child: Column(
+                          children: [
+                            Form(
+                              key: formKey,
+                              child: Column(
+                                children: [
+                                  //email
+                                  TextFormField(
+                                    controller: emailController,
+                                    validator: (val) => val =="" ? "Please write email" : null,
+                                    decoration: InputDecoration(
+                                      prefixIcon: const Icon(
+                                        Icons.email,
+                                        color: Colors.black,
+                                      ),
+                                      hintText: "email...",
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: BorderSide(
+                                          color: Colors.white60,
+                                        ),
+                                      ),
+                                      enabledBorder:  OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: BorderSide(
+                                          color: Colors.white60,
+                                        ),
+                                      ),
+                                      focusedBorder:  OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: BorderSide(
+                                          color: Colors.white60,
+                                        ),
+                                      ),
+                                      disabledBorder:  OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                        borderSide: BorderSide(
+                                          color: Colors.white60,
+                                        ),
+                                      ),
+                                      contentPadding: const EdgeInsets.symmetric(
+                                        horizontal: 14,
+                                        vertical: 6,
+                                      ),
+                                      fillColor: Colors.white, //fill the withe color in the  text box
+                                      filled: true,
                                     ),
                                   ),
-                                  enabledBorder:  OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
-                                    ),
-                                  ),
-                                  focusedBorder:  OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
-                                    ),
-                                  ),
-                                  disabledBorder:  OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
-                                    ),
-                                  ),
-                                  contentPadding: const EdgeInsets.symmetric(
-                                    horizontal: 14,
-                                    vertical: 6,
-                                  ),
-                                  fillColor: Colors.white, //fill the withe color in the  text box
-                                  filled: true,
-                                ),
-                              ),
 
-                              SizedBox(height: 18,),//create text box height
+                                  const SizedBox(height: 18,),//create text box height
 
-                              //password
-                              TextFormField(
-                                controller: passwordController,
-                                obscureText: isObsecure.value,
-                                validator: (val) => val =="" ? "Please write password" : null,
-                                decoration: InputDecoration(
-                                  prefixIcon: const Icon(
-                                    Icons.vpn_key_sharp,
-                                    color: Colors.black,
-                                  ),
-                                  suffixIcon: Obx(              //hidden password icon
-                                      ()=> GestureDetector(
-                                        onTap: ()
-                                        {
-                                          isObsecure.value = !isObsecure.value; // hiden the password
-                                        },
-                                        child: Icon(
-                                          isObsecure.value ? Icons.visibility_off : Icons.visibility,
-                                          color: Colors.black,
+                                  //password
+                                  Obx(
+                                      ()=> TextFormField(
+                                        controller: passwordController,
+                                        obscureText: isObsecure.value,
+                                        validator: (val) => val =="" ? "Please write password" : null,
+                                        decoration: InputDecoration(
+                                          prefixIcon: const Icon(
+                                            Icons.vpn_key_sharp,
+                                            color: Colors.black,
+                                          ),
+                                          suffixIcon: Obx(              //hidden password icon
+                                                ()=> GestureDetector(
+                                              onTap: ()
+                                              {
+                                                isObsecure.value = !isObsecure.value; // hiden the password
+                                              },
+                                              child: Icon(
+                                                isObsecure.value ? Icons.visibility_off : Icons.visibility,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ),
+                                          hintText: "passsword...",
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(30),
+                                            borderSide: BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          enabledBorder:  OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(30),
+                                            borderSide: BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          focusedBorder:  OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(30),
+                                            borderSide: BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          disabledBorder:  OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(30),
+                                            borderSide: BorderSide(
+                                              color: Colors.white60,
+                                            ),
+                                          ),
+                                          contentPadding: const EdgeInsets.symmetric(
+                                            horizontal: 14,
+                                            vertical: 6,
+                                          ),
+                                          fillColor: Colors.white, //fill the withe color in the  text box
+                                          filled: true,
                                         ),
                                       ),
                                   ),
-                                  hintText: "passsword...",
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
-                                    ),
-                                  ),
-                                  enabledBorder:  OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
-                                    ),
-                                  ),
-                                  focusedBorder:  OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
-                                    ),
-                                  ),
-                                  disabledBorder:  OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide: BorderSide(
-                                      color: Colors.white60,
-                                    ),
-                                  ),
-                                  contentPadding: const EdgeInsets.symmetric(
-                                    horizontal: 14,
-                                    vertical: 6,
-                                  ),
-                                  fillColor: Colors.white, //fill the withe color in the  text box
-                                  filled: true,
-                                ),
-                              ),
 
-                            ],
-                          ),
+                                  const SizedBox(height: 18,),
+                                   //button
+                                  Material(     // loging button create
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(30),
+                                    child: InkWell(
+                                      onTap: ()
+                                      {
+
+                                      },
+                                      borderRadius: BorderRadius.circular(30),
+                                      child: const Padding(
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: 10,
+                                          horizontal: 28,
+                                        ),
+                                        child: Text(
+                                          "Login",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 ],
