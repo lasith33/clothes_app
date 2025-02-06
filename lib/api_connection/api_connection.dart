c396ae480fac8b09779ea1780f793c7c803d1b9c
lasith chamuditha <125144995+lasith33@users.noreply.github.com>
2025-02-06 23:00:16 +0530

    create bottom navigation bar

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,6 +1,6 @@
 class API
 {
-  static const hostConnect = "http://192.168.109.231/api_clothes_store";
+  static const hostConnect = "http://192.168.160.231/api_clothes_store";
   static const hostConnectUser = "$hostConnect/user";
   //singUp user
   static const validateEmail = "$hostConnect/user/validate_email.php";
